e57f881ed75323141be164b5c347c12eb24332ca
Guy Strauss <gstrauss@akamai.com>
2025-01-16 15:47:16 +0200

    Adding files

---
+++ app/assigment2/perspective_warping_empty.py
@@ -0,0 +1,23 @@
+# ======= imports
+
+# ======= constants
+
+# === template image keypoint and descriptors
+
+# ===== video input, output and metadata
+
+# ========== run on all frames
+while True:
+    # ====== find keypoints matches of frame and template
+    # we saw this in the SIFT notebook
+
+    # ======== find homography
+    # also in SIFT notebook
+
+    # ++++++++ do warping of another image on template image
+    # we saw this in SIFT notebook
+
+    # =========== plot and save frame
+    pass
+
+# ======== end all
